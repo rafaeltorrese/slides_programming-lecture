5059162f3d1355eac0c6ea2367f1f39b2a1dc050
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-08-04 16:11:11 -0500

    column to integer function was added to load_data_better.py

--- 03_executing-programs/lab01/load_data_better.py
+++ 03_executing-programs/lab01/load_data_better.py
@@ -6,19 +6,34 @@ def load_csv(filename):
         data = [*csv.reader(file)]
     return data
 
-def column_to_string(dataset, column):
+def column_to_float(dataset, column):
     for row in dataset:
         row[column] = float(row[column].strip())
 
+def column_to_integer(datasets, column):
+    'Function that transforms string to an integer'
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = {value:i for i, value in enumerate(unique)}
+    print(lookup)
+    for row in dataset:
+        row[column] = lookup[ row[column] ]
+    return lookup
+
+
 if __name__ == "__main__":
     # Load pima-indians-diabetes dataset
-    filename = "03_executing-programs\lab01\pima-indians-diabetes.csv"
+    filename = r"03_executing-programs\data\iris.csv"
     dataset = load_csv(filename)
     print(f"Loaded data file {filename} with {len(dataset)} rows and {len(dataset[0])} columns")
-    print(dataset[0])
 
     # convert string columns to float
     ncols = len(dataset[0])
-    for i in range(ncols):
-        column_to_string(dataset, i)
+    for i in range(ncols - 1):
+        column_to_float(dataset, i)
     print(dataset[0])
+
+    # convert class column to integer
+    lookup = column_to_integer(dataset, 4)
+    print(dataset[0])
+    print(lookup)
