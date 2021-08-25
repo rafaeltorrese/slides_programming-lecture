3477d3522817f2d487abe6f2564278ddd22a2a85
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-08-25 08:38:49 -0500

    Plotting horizontal bars

---
+++ 04_plotting/exercise64a.py
@@ -0,0 +1,22 @@
+#%%
+import csv
+import matplotlib.pyplot as plt
+#%%
+
+with open('data/students.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    data = [*csv_reader]
+
+grades, students_count = [*zip(*data)]
+students_count = [int(number) for number in students_count]
+
+colors = ('green', *tuple(['gray'] * 4), 'red')
+#%%
+plt.title('Grades Bar Plot For Biology Class')
+plt.xlabel('Grade')
+plt.ylabel('Num Students')
+
+# Horizontal Bar
+plt.barh(grades, students_count, color=colors)
+plt.show()
